e6cab3011aa95031b7ee388662f6d3ca6e1c5214
mahbus <2107.subham@gmail.com>
2026-07-13 14:34:31 +0600

    Refactor code structure for improved readability and maintainability

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import { siteUrl, SITE_NAME } from "@/lib/site";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -41,13 +42,16 @@ export default function RootLayout({
                 suppressHydrationWarning
             >
                 {children}
-                <SpeedInsights />
+
 
                 <Toaster
                     position="top-right"
                     swipeDirections={["right"]}
                     richColors={true}
                 />
+                <SpeedInsights />
+                <Analytics />
+
             </body>
         </html>
     );
